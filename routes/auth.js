957395358b9d87959d15c75de8f772a3ab1727f5
sajdakabir <sajda.kbir.com>
2022-05-04 21:44:59 +0530

    added login and register router

---
+++ routes/auth.js
@@ -0,0 +1,52 @@
+const router=require("express").Router();
+const User=require("../models/User");
+const CryptoJS=require("crypto-js");
+const jwt=require("jsonwebtoken");
+// register
+router.post("/register",async(req,res)=>{
+    const newUser=new User({
+        username:req.body.username,
+        email:req.body.email,
+        password:CryptoJS.AES.encrypt(req.body.password,process.env.PASS_SEC).toString(),
+    });
+    try{
+        const saveUser=await newUser.save();
+       res.status(200).json(saveUser);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+
+// LOgin
+
+router.post("/login",async(req,res)=>{
+    try{
+        const user=await User.findOne({username:req.body.username});
+        !user && res.status(400).json("Wrong Credential!")
+        const hashedPassword=CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+            );
+            const originalPassword=hashedPassword.toString(CryptoJS.enc.Utf8);
+            originalPassword !==req.body.password && 
+            res.status(400).json("Wrong Credential!");
+            const accessToken=jwt.sign({
+                id:user._id, 
+                isAdmin:user.isAdmin,
+            },
+            process.env.JWT_SEC,
+            {expiresIn:"4d"}
+            )
+
+
+            const {password, ...others}=user._doc;
+
+            res.status(200).json({others,accessToken});
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+
+module.exports=router;
